9ace2452dae15538036d769edc20f142c5685681
Austin Goetz <a.goetz09@gmail.com>
2020-09-22 20:19:23 -0600

    adds custom error messages for result type

--- RedditMASTER/HelpersAndExtensions/PostError.swift
+++ RedditMASTER/HelpersAndExtensions/PostError.swift
@@ -13,4 +13,19 @@ enum PostError: LocalizedError {
     case noData
     case thrownImageError(Error)
     case unableToDecode
+    
+    var errorDescription: String {
+        switch self {
+        case .invalidURL:
+            return "The server failed to reach the necessary URL"
+        case .thrownError(let error):
+            return "There was an error: \(error.localizedDescription)"
+        case .noData:
+            return "There was no data found"
+        case .thrownImageError(let error):
+            return "There was an error with an image: \(error.localizedDescription)"
+        case .unableToDecode:
+            return "There was an error when trying to load the data"
+        }
+    }
 }
